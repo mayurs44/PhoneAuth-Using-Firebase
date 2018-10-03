c18d7bbf05f233bb6ae1e80feaf239516d8d6551
Mayur Shinde <mayur.shinde129@gmail.com>
2018-10-03 16:35:40 +0530

    First Commit

---
+++ app/src/main/java/in/inviera/mobileauth/MyApplication.java
@@ -0,0 +1,14 @@
+package in.inviera.mobileauth;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
